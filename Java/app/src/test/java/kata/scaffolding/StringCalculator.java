aad74a810ce0f9cffed65f7bda818ffdd846480c
Omar <omar.piga@wallstreetfootball.io>
2023-11-03 17:03:30 +0100

    4th step - Omar

--- Java/app/src/test/java/kata/scaffolding/StringCalculator.java
+++ Java/app/src/test/java/kata/scaffolding/StringCalculator.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 public class StringCalculator {
 
   public static int add(String input) {
-    if (input.isBlank()) {
-      return 0;
+    String regex = "[\n,]";
+    String newInput = input;
+
+    if (input.startsWith("//")) {
+      newInput = input.substring(4);
+      regex = String.valueOf(input.charAt(2));
     }
 
-    String[] numbers = input.split(",");
-
-    return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+    String[] numbers = newInput.split(regex);
+    return Stream.of(numbers)
+      .filter(not(String::isBlank))
+      .mapToInt(Integer::parseInt).sum();
   }
 }
 
